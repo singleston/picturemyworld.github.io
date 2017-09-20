257ca975b656fda7034826996bf1522fce83f430
Kevin Delord <delord.kevin@gmail.com>
2017-09-20 06:54:31 +0200

    fix scroll to item for safari and firefox

--- js/custom/scroll_to_item.js
+++ js/custom/scroll_to_item.js
@@ -5,12 +5,10 @@ function ready(f){/in/.test(document.readyState)?setTimeout('ready('+f+')',9):f(
 ready(function(){
 	let identifier = window.location.hash.substring(1);
 	let element = document.getElementById(identifier);
-	if (element) {
-		// Pure Javascript scroll to with alignment.
-		// https://stackoverflow.com/a/36499256/2790648
-		const elementRect = element.getBoundingClientRect();
-		const absoluteElementTop = elementRect.top + window.pageYOffset;
-		const middle = absoluteElementTop - 90; // `-90`in order to show the element under the nav bar, not behind it.
+	let header_height = document.getElementsByClassName("header_container")[0].getBoundingClientRect().height
+	if (element && header_height) {
+		const elementTop = element.getBoundingClientRect().top;
+		const middle = elementTop - header_height - 20; // Extra margin
 		window.scrollTo(0, middle);
 	}
 });
