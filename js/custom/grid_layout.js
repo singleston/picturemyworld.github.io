abceaf1bdf65ddd3aae785a24752e068aab46313
Kevin Delord <delord.kevin@gmail.com>
2017-09-26 17:44:29 +0200

    Grid: add background color and improve lazyloading

--- js/custom/grid_layout.js
+++ js/custom/grid_layout.js
@@ -1,15 +1,20 @@
 /**
  * grid_layout.js
  *
- * Attach javascript to grid-gallery.
- * Initialize grid and update custom height for each grid item.
+ * Attach javascript to grid-gallery:
+ * - Initialize grid and update custom height for each grid item.
+ * - Initialize the slideshow content.
+ * - Configure the lazy loading of the images.
+ * - Initialize the infinite scroll.
  */
+
+// The distance out of the viewport, in pixel, before which to start loading the images and the next page loading.
+const bottomPadding = 200;
 
 // Mansory custom grid and Slideshow layout
 var grid = new CBPGridGallery(document.getElementById('grid-gallery'));
 
 refreshGridLayout = function() {
-
 	// Calculate and set the 'responsive' display size for the each thumbnails in the grid.
 	[].forEach.call(document.getElementsByClassName('grid_thumbnail'), function(element) {
 		let ratio = element.dataset.ratio
@@ -25,8 +30,20 @@ refreshGridLayout = function() {
 
 	// Mazy loading
 	var myLazyLoad = new LazyLoad({
-		threshold: 0
+		threshold: bottomPadding
 	});
 };
 
+// Initial custom grid layout.
 refreshGridLayout();
+
+// Load jScroll and simulate infinite scroll.
+$('.grid-gallery').jscroll({
+	debug: false,
+	nextSelector: 'a.see_more',
+	contentSelector: '.grid_item',
+	slideshowSelector: '.slideshow_item',
+	spinnerClass: 'infinite-spinner',
+	padding: bottomPadding,
+	callback: refreshGridLayout
+});
