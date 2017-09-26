32f57fc1624f594c808e41af2878c599ee37633c
Kevin Delord <delord.kevin@gmail.com>
2017-09-26 03:40:18 +0200

    Fix first extra load on start

--- js/custom/jquery.jscroll.js
+++ js/custom/jquery.jscroll.js
@@ -85,7 +85,7 @@
                         innerTop = $inner.length ? $inner.offset().top : 0,
                         iTotalHeight = Math.ceil(iTopHeight - innerTop + _$scroll.height() + iContainerTop);
 
-                    if (!data.waiting && iTotalHeight + _options.padding >= $inner.outerHeight()) {
+                    if (!data.waiting && iTotalHeight + _options.padding >= $inner.outerHeight() && $inner.outerHeight() > 0) {
                         _debug('info', 'jScroll:', $inner.outerHeight() - iTotalHeight, 'from bottom. Loading next request...');
                         return _load();
                     }
